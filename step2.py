ca01df8e25ee35fad3acac2a3654a4eff7e49f46
Fernandez Pius <P10158746@capita.co.uk>
2025-08-12 15:43:49 +0100

    Added logic to the program

--- step2.py
+++ step2.py
@@ -1,8 +1,30 @@
+result1 = 0
+result2 = 0
+result3 = 0
+result4 = 0
+Total = 0
+
 with open("numbers.txt", "r") as file:
     numbers = file.read().splitlines()
     for number in numbers:
-        print(number)
-  
+     calc,operator,number1,number2 = number.split()  
+     number1 = int(number1)
+     number2 = int(number2)
+    if operator =="+":
+        result1 += int(number1) + int(number2) 
+        print("The result is:", result1)
+    elif operator =="-":
+        result2 += int(number1) - int(number2)
+        print("The result is:", result2)
+    elif operator =="/":
+        result3 += int(number1) / int(number2)
+        print("The result is:", result3)
+    else: 
+        operator =="*"
+        result4 += int(number1) * int(number2)
+    print("The result is:", result4)
 
+Total = result1 + result2 + result3 +result4
+print("The result is:", Total)
   
 #print(numbers)
