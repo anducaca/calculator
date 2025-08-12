ed57b78aa2d8dbd3259f3456207c79795516f5d7
Andrew McCormack <andycaca@gmail.com>
2025-08-12 14:17:39 +0100

    handover

--- calculator.py
+++ calculator.py
@@ -1,1 +1,12 @@
 print("Welcome to the Calculator!")
+
+# Get the user's answer
+user_operator = input("Your operator (+, -, /, *): ")
+user_firstnumber = input("Your first (whole) number:")
+user_secondnumber = input("Your second (whole) number:")
+
+#print(user_firstnumber)
+
+if user_operator =="+":
+    result = int(user_firstnumber) + int(user_secondnumber)
+    print(result)
